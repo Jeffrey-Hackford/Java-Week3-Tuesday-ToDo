ba08447f912fda2da0d28a377a1e8d20d3954d8a
Ian lesikar and Jeffrey Hackford <jeffrey@>
2016-05-03 15:12:51 -0700

    finish front end integreation

---
+++ src/main/java/Category.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+import java.util.List;
+import org.sql2o.*;
+import java.util.ArrayList;//
+
+public class Category {
+  private int id;
+  private String name;
+  private static ArrayList<Category> instances = new ArrayList<Category>();//
+  private ArrayList<Task> mTasks;//
+  private int mId;//
+
+  public Category(String name) {
+    this.name = name;
+    instances.add(this);//
+    mId = instances.size();//
+    mTasks = new ArrayList<Task>();//
+  }
+
+  public void addTask(Task task) {
+    mTasks.add(task);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Category> all() {
+    String sql = "SELECT id, name FROM Categories";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Category.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherCategory) {
+    if (!(otherCategory instanceof Category)) {
+      return false;
+    } else {
+      Category newCategory = (Category) otherCategory;
+      return this.getName().equals(newCategory.getName()) &&
+             this.getId() == newCategory.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO Categories(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Category find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM Categories where id=:id";
+      Category category = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Category.class);
+      return category;
+    }
+  }
+
+  public List<Task> getTasks() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM tasks where categoryId=:id";
+      return con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeAndFetch(Task.class);
+    }
+  }
+
+
+}
